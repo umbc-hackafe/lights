2ea273b26687b278f88a43032d4151a69d90b43e
Dylan Whichard <dylan@whichard.com>
2015-10-28 19:30:23 -0400

    Finish lights UI

--- web/lights.js
+++ web/lights.js
@@ -52,6 +52,14 @@
 
 }());
 
+function $f(sel, frame) {
+    return $(sel + ":data(frame==" + frame + ")");
+}
+
+function $fs(sel, frame, seg) {
+    return $(sel + ":data(frame==" + frame + ",segment==" + seg + ")");
+}
+
 function addFrame() {
     var parent = $("#animation-editor-form");
     var frames = $("#animation-editor").data('frames');
@@ -59,11 +67,14 @@ function addFrame() {
     parent.append(
 	$("<div>").addClass("frame-container").data("frame", frames+1).append(
 	    $("<div>").addClass("frame-options").data("frame", frames+1).append(
+		$("<input>").attr('type', 'button').data('frame', frames+1).val('X').click(function() {
+		    $(".frame-container:data(frame==" + $(this).data('frame') + ")").remove();
+		}),
 		$("<span>").addClass("frame-number").data("frame", frames+1).text("Frame #" + (frames+1)),
 		document.createTextNode("&bull;"),
 		$("<label>").append(
 		    document.createTextNode("Duration: "),
-		    $("<input>").attr("type", "number").val(1).attr("max", 3600).attr("min", .042).data("frame", frames+1),
+		    $("<input>").addClass('frame-duration').attr("type", "number").val(1).attr("max", 3600).attr("min", .042).data("frame", frames+1),
 		    document.createTextNode("s")
 		),
 		$("<input>").attr('type', 'button').data('frame', frames+1).val("+").click(function() {
@@ -98,15 +109,68 @@ function addSegment(frame) {
 		$("<input>").attr('type', 'button').val('X').click(function(){
 		    $(".segment-container:data(frame==" + frame + ",segment==" + segment + ")").remove();
 		}),
-		$("<input>").attr('type', 'color').val('#FFFFFF').data('frame', frame).data('segment', segment),
+		$("<input>").addClass('segment-color').attr('type', 'color').val('#FFFFFF').data('frame', frame).data('segment', segment),
 		$("<input>").addClass('segment-brightness').attr('type', 'range').attr('min', 0).attr('max', 255).val(255).data('frame', frame).data('segment', segment),
-		$("<input>").attr('type', 'checkbox').addClass('segment-all-brightness').data('frame', frame).data('segment', segment)
+		$("<input>").attr('type', 'checkbox').addClass('segment-all-brightness').data('frame', frame).data('segment', segment).click(function() {
+		    $(".light-control:data(frame==" + frame + ",segment==" + $(this).data("segment") + ",frame==" + $(this).data("frame") + ")").prop('disabled', $(this).is(':checked')).val(False);
+		})
 	    ),
 	    $("<div>").addClass('segment-lights').append(checkboxes)
 	)
     );
 }
 
+function serializeAnimation() {
+    var frames = $("#animation-editor").data('frames');
+    var result = {
+	"__type__": "Animation",
+	"duration": 0,
+	"steps": []
+    };
+
+    $(".frame-duration").each(function() {
+	result["duration"] += Number($(this).val());
+    });
+
+    for (var frame = 1; frame <= frames; frame++) {
+	if (!$(".frame-container:data(frame==" + frame + ")"))
+	    continue;
+
+	var frameObj = {
+	    "__type__": "Frame",
+	    "segments": [],
+	    "duration": Number($(".frame-duration:data(frame==" + frame + ")").val())
+	};
+
+	var segments = $(".frame-segment-list:data(frame==" + frame + ")").data('segments');
+	for (var segment = 1; segment <= segments; segment++) {
+	    if (!$(".segment-container:data(frame==" + frame + ",segment==" + segment + ")"))
+		continue;
+
+	    var segObj = {
+		"__type__": "Segment",
+		"brightness": Number($(".segment-brightness:data(frame==" + frame + ",segment==" + segment + ")").val()),
+		"color": parseInt($(".segment-color:data(frame==" + frame + ",segment==" + segment + ")").val().substring(1), 16),
+		"lights": []
+	    };
+
+	    $fs(".light-control:checked:enabled", frame, segment).each(function() {
+		segObj["lights"].push(Number($(this).data('light')));
+	    });
+
+	    frameObj["segments"].push(segObj);
+	}
+	result["steps"].push(frameObj);
+    }
+
+    return result;
+}
+
+function saveAnimation() {
+    $.post("/add_animation", JSON.stringify(serializeAnimation()));
+}
+
 $(function() {
     $("#add-frame").click(function(){addFrame();});
+    $("#save-animation").click(function(){saveAnimation();});
 });
